0b4ae53af40850b7c97240be4047a67ad661b450
Yezir971 <james_ahmedaly@yahoo.com>
2025-05-26 18:07:27 +0200

    feat : add all font and color from figma

--- lib/styles.dart
+++ lib/styles.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 
+/// Alternate primary600 or #112236
 const primaryBase = Color.fromARGB(100, 17,34,54);
 const secondaryBase = Color.fromARGB(100, 243, 233, 11);
-// titre h2
+const tertiary100 = Color.fromARGB(100,238,241,245 );
+const quinaryBase = Color.fromARGB(100,2,2,2 );
+/// Alternate quatrenary700
+const quatrenaryBase = Color.fromARGB(100,255,254,255 );
+const primary700 = Color.fromARGB(100,3,19,28 );
+const septenary100 = Color.fromARGB(100,217,226,238 );
+/// Title h2
 const title = TextStyle(
   fontSize: 60,
   fontFamily: 'Anton',
