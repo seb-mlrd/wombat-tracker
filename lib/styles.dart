884cbbb029f82be45115f3963c4b9ca196b7f775
Seb-mlrd <sebmlrd06@gmail.com>
2025-05-26 17:05:12 +0200

    ajout du style et de la fonts

---
+++ lib/styles.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+const primaryBase = Color.fromARGB(100, 17,34,54);
+const secondaryBase = Color.fromARGB(100, 243, 233, 11);
+// titre h2
+const title = TextStyle(
+  fontSize: 60,
+  fontFamily: 'Anton',
+);
+
+const subTitle = TextStyle(
+  fontSize: 40,
+  fontFamily: 'Anton',
+);
+
+const subSubTitle = TextStyle(
+  fontSize: 16,
+  fontFamily: 'Anton',
+);
+
+const addStick = TextStyle(
+  fontSize: 12,
+  fontFamily: 'Anton',
+);
+
+const bodyText = TextStyle(
+  fontSize: 36,
+  fontFamily: 'Roboto',
+);
+
+const bodyTextMedium = TextStyle(
+  fontSize: 16,
+  fontFamily: 'Roboto',
+);
+
+const bodyTextSmall = TextStyle(
+  fontSize: 8,
+  fontFamily: 'Roboto',
+);
